a74e89e67ff56b4a62f08c5cecd5cfdca1efc088
Your NameBruno <brunomalbano@gmail.com>
2021-04-20 16:17:15 -0300

    Criação de controllers e alteração das rotas.

--- src/routes.ts
+++ src/routes.ts
@@ -1,21 +1,9 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
+const settingsController = new SettingsController();
 
-routes.post('/settings', async (req, res) => {
-    const { chat, username } =  req.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    });
-
-    await settingsRepository.save(settings);
-
-    return res.json(settings);
-});
+routes.post('/settings', settingsController.create);
 
 export { routes };
