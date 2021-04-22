096ac7fd356175a04188ab9358b56d26998898f0
Your NameBruno <brunomalbano@gmail.com>
2021-04-22 03:15:08 -0300

    Ajustes nos services

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -1,12 +1,17 @@
-import { getCustomRepository } from "typeorm"
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
 import { UsersRepository } from "../repositories/UsersRepository"
 
 
 class UsersService {
-    async create(email: string) {
-       const usersRepository = getCustomRepository(UsersRepository);
-       
-       const userExists = await usersRepository.findOne({
+    private usersRepository: Repository<User>;
+
+    constructor() {
+        this.usersRepository = getCustomRepository(UsersRepository);
+    }
+
+    async create(email: string) {       
+       const userExists = await this.usersRepository.findOne({
            email
        });
 
@@ -14,11 +19,11 @@ class UsersService {
            return userExists;
        }
 
-       const user = usersRepository.create({
+       const user = this.usersRepository.create({
            email
        });
 
-       await usersRepository.save(user);
+       await this.usersRepository.save(user);
 
        return user;
     }
