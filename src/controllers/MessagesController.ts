da4ec82c1fbabeed44a6ff834a1ed958727667cb
Your NameBruno <brunomalbano@gmail.com>
2021-04-22 03:05:36 -0300

    Criando a rota, controller, service, entidade e repositório para messages

---
+++ src/controllers/MessagesController.ts
@@ -0,0 +1,29 @@
+import { Request, response, Response } from "express";
+import { MessagesServices } from "../services/MessagesService";
+
+class MessagesController {
+    async create(req: Request, res: Response) {
+        const { admin_id, text, user_id } = req.body;
+        const messagesService = new MessagesServices();
+        
+        const message = await messagesService.create({
+            admin_id,
+            text,
+            user_id
+        });
+
+        return res.json(message);
+    }
+
+    async showByUser(req: Request, res: Response) {
+        const { id } = req.params;
+        
+        const messagesService = new MessagesServices();
+        
+        const list = await messagesService.listByUser(id);
+
+        return res.json(list);
+    }
+}
+
+export { MessagesController };
