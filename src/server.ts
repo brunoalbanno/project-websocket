3d545351b1facb1e6917452de8013cb72e52d824
Your NameBruno <brunomalbano@gmail.com>
2021-04-23 13:43:15 -0300

    Coletando informações pela conexão client

--- src/server.ts
+++ src/server.ts
@@ -1,32 +1,5 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server, Socket} from 'socket.io';
-
-import './database';
-import { routes } from './routes';
-import path from 'path';
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.set('views', path.join(__dirname, '..', 'public'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-
-app.get('/pages/client', (req, res) => {
-    return res.render('html/client.html');
-})
-
-const http = createServer(app);
-const io = new Server(http);
-
-io.on('connection', (socket: Socket) => {
-    console.log('Se conectou', socket.id);
-})
-
-app.use(express.json());
-
-app.use(routes);
+import { http } from './http';
+import './websocket/client';
 
 http.listen(3333, () => {
     console.log('Server is running on port 3333.')
